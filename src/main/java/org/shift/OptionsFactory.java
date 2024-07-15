2b0e02a1d7c777f5a7eac3c01d2584828027bf4c
Sawyron <s4pakvv@gmail.com>
2024-07-15 17:32:03 +0700

    add cli options setup

---
+++ src/main/java/org/shift/OptionsFactory.java
@@ -0,0 +1,38 @@
+package org.shift;
+
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
+import java.nio.file.Path;
+
+public class OptionsFactory {
+    public Options provideOptions() {
+        var options = new Options();
+        Option prefix = Option.builder("p")
+                .longOpt("prefix")
+                .argName("prefix")
+                .hasArg()
+                .build();
+        Option output = Option.builder("o")
+                .longOpt("output")
+                .argName("output")
+                .hasArg()
+                .type(Path.class)
+                .build();
+        Option append = Option.builder("a")
+                .longOpt("append")
+                .build();
+        Option simpleStats = Option.builder("s")
+                .longOpt("simple")
+                .build();
+        Option fullStats = Option.builder("f")
+                .longOpt("full")
+                .build();
+        options.addOption(prefix);
+        options.addOption(output);
+        options.addOption(append);
+        options.addOption(simpleStats);
+        options.addOption(fullStats);
+        return options;
+    }
+}
---
+++ src/test/java/org/shift/OptionsFactoryTest.java
@@ -0,0 +1,54 @@
+package org.shift;
+
+import org.apache.commons.cli.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class OptionsFactoryTest {
+    private CommandLineParser parser;
+    private OptionsFactory optionsFactory;
+
+    @BeforeEach
+    void setUp() {
+        parser = new DefaultParser();
+        optionsFactory = new OptionsFactory();
+    }
+
+    @Test
+    void testProvideOptions() {
+        Options options = optionsFactory.provideOptions();
+        String command = "-p pre -a -s -f in1.txt in2.txt";
+        try {
+            CommandLine line = parser.parse(options, command.split(" "));
+            String prefix = line.getOptionValue("prefix");
+            assertEquals("pre", prefix);
+            assertTrue(line.hasOption("append"));
+            assertTrue(line.hasOption("simple"));
+            assertTrue(line.hasOption("full"));
+            String[] args = line.getArgs();
+            assertEquals(2, args.length);
+            assertEquals("in1.txt", args[0]);
+            assertEquals("in2.txt", args[1]);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testOutputOption() {
+        Options options = optionsFactory.provideOptions();
+        String command = "-o ./res";
+        try {
+            CommandLine line = parser.parse(options, command.split(" "));
+            Path output = line.getParsedOptionValue("o");
+            assertTrue(output.endsWith("res"));
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
