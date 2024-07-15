7dbee7ac65caf1b675f7b5294703e7e9eebdf6eb
Sawyron <s4pakvv@gmail.com>
2024-07-15 17:32:52 +0700

    add ValueSorter class

---
+++ src/main/java/org/shift/ValueSorter.java
@@ -0,0 +1,31 @@
+package org.shift;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class ValueSorter {
+    private final List<ValueContainer<?>> containers;
+
+    public ValueSorter(Collection<ValueContainer<?>> containers) {
+        this.containers = new ArrayList<>(containers);
+    }
+
+    public void sort(InputStream in) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+            reader.lines().forEach(this::tryAddValue);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void tryAddValue(String token) {
+        for (ValueContainer<?> container : containers) {
+            if (container.canParse(token)) {
+                container.add(token);
+                break;
+            }
+        }
+    }
+}
