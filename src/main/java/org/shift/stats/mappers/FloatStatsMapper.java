0cb14dbd5599e43c46bf65488ec943fb8777d39c
Sawyron <s4pakvv@gmail.com>
2024-07-17 02:59:06 +0700

    replace manual float statistics with DoubleSummaryStatistics

--- src/main/java/org/shift/stats/mappers/FloatStatsMapper.java
+++ src/main/java/org/shift/stats/mappers/FloatStatsMapper.java
@@ -2,27 +2,16 @@ package org.shift.stats.mappers;
 
 import org.shift.stats.models.FloatStats;
 
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.function.Function;
 
 public class FloatStatsMapper implements Function<List<Double>, FloatStats> {
     @Override
     public FloatStats apply(List<Double> doubles) {
-        double max = doubles.stream()
+        DoubleSummaryStatistics stats = doubles.stream()
                 .mapToDouble(Double::doubleValue)
-                .max()
-                .orElse(0);
-        double min = doubles.stream()
-                .mapToDouble(Double::doubleValue)
-                .min()
-                .orElse(0);
-        double sum = doubles.stream()
-                .mapToDouble(Double::doubleValue)
-                .sum();
-        double average = doubles.stream()
-                .mapToDouble(Double::doubleValue)
-                .average()
-                .orElse(0);
-        return new FloatStats(max, min, sum, average);
+                .summaryStatistics();
+        return new FloatStats(stats.getMax(), stats.getMin(), stats.getSum(), stats.getAverage());
     }
 }
