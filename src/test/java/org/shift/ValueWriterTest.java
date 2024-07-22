7013ee280ceed10d3f5e28d187785536366b1ac0
Sawyron <s4pakvv@gmail.com>
2024-07-23 01:00:30 +0700

    add ValueWriter tests

---
+++ src/test/java/org/shift/ValueWriterTest.java
@@ -0,0 +1,33 @@
+package org.shift;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValueWriterTest {
+
+    @Test
+    void whenGetNonEmptyContainer_thenWriteValuesOnSeparateLines() {
+        ValueContainer<String> container = new ValueContainer<>(_ -> true, token -> token);
+        container.add("one");
+        container.add("two");
+        container.add("three");
+        var writer = new ValueWriter(container);
+        var output = new ByteArrayOutputStream();
+        assertDoesNotThrow(() -> writer.write(output));
+        String result = output.toString();
+        String expected = String.join(System.lineSeparator(), container.tokens().toList());
+        expected += System.lineSeparator();
+        assertEquals(expected, result);
+        assertEquals(3, container.getSize());
+    }
+
+    @Test
+    void whenContainerHasNoElements_thenIsEmptyTrue() {
+        ValueContainer<String> container = new ValueContainer<>(_ -> true, token -> token);
+        var writer = new ValueWriter(container);
+        assertTrue(writer.isEmpty());
+    }
+}
